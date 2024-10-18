815f78d7474f796a948b851536efd2850fad88a3
tanvirgb <tspramanik123@gmail.com>
2024-10-18 13:05:40 +0600

    Make Create Post Page

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 4000;
+let posts = [];
 
-app.use(express.static("public")); // Serve static files on port 4000 (default  port 4000 if not specified)
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+// Middleware for parsing URL-encoded data (form data)
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Middleware for parsing JSON data (application/json)
+app.use(bodyParser.json());
+
+// app.post("/api/create", (req, res) => {
+//   const post = {
+//     id: ++id,
+//     type: req.body.type || "Not specified",
+//     author: req.body.author || "Anonymous",
+//     title: req.body.title,
+//     date: new Date().toISOString(),
+//     content: req.body.content,
+//   };
+//   posts.push(post);
+//   res.json(post);
+// });
+
+// Start the server on port 4000 and log a success message.
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`API is running on port ${port}`);
 });
---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "http://localhost:4000";
+
+// Serve static files on port 3000 (default  port 3000 if not specified)
+app.use(express.static("public"));
+
+// Middleware for parsing URL-encoded data (form data)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware for parsing JSON data (application/json)
+app.use(bodyParser.json());
+
+// GET route for the homepage. Renders the index.ejs file.
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+
+// app.post("/create", async (req, res) => {
+//   try {
+//     const response = await axios.post(`{API_URL}/api/create`, req.body);
+//     console.log(response.data);
+//     res.redirect("/");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error creating a new post");
+//   }
+// });
+
+// Start the server on port 3000 and log a success message.
+app.listen(3000, () => {
+  console.log(`Backend server is running on http://localhost:${port}`);
+});
